cc6f531a8fcc5664bca1675bef3d2b5fe976dd56
gabrielgil245 <gabrielgil245@gmail.com>
2021-03-17 22:21:10 -0500

    Initiated basic UI including navbar and sidebar. Fetch categories method retrieves the information from said table and displays the names to the sidebar.

--- src/App.js
+++ src/App.js
@@ -1,24 +1,64 @@
-import logo from './logo.svg';
 import './App.css';
+import React, {useState, useEffect} from 'react';
+
 
 function App() {
+
+  const [categories, setCategories] = useState([]);
+  const [selectedCategory, setSelectedCategory] = useState();
+
+  const fetchCategories = async () => {
+    console.log("This function will fetch the categories");
+    let res = await fetch('http://localhost:3000/api/v1/categories');
+    let data = await res.json();
+    console.log(data);
+    setCategories(data);
+  }
+
+  useEffect(() => {
+    fetchCategories();
+  }, [])
+
+  useEffect(() => {
+    // fetchQuestions() - // TODO - Fetch and show the questions
+  }, [selectedCategory])
+
+  // Empty square brackets leads to running once on load
+  // If someStateVariable changes, this function will load
+  // Use effect may be dependent for multiple variables
+  
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <div className={"grid grid-cols-12"}>
+        <div className={"col-span-full border p-4"}>
+          <h1 className={'text-center text-3xl'}>
+            App Title
+          </h1>
+        </div>
+      </div>
+        
+      <div className={"grid grid-cols-12"}>
+        <div className={"col-span-full md:col-span-3 lg:col-span-2 border p-4"}>
+            <ul>
+              {categories.map((category, index) => {
+                return <li key={index} className={category.id == selectedCategory ? 
+                  "border p-4 cursor-pointer bg-gray-300" : "border p-4 cursor-pointer"} 
+                  onClick={() => {
+                  setSelectedCategory(category.id);
+                }}>
+                  {category.name}
+                </li>
+              })}
+            </ul>
+        </div>
+            
+        <div className={"col-span-full md:col-span-9 lg:col-span-10 border p-4"}>
+          <h1>
+            Questions List
+          </h1>
+        </div>
+      </div>
+    </>
   );
 }
 
