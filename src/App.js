ae7e83c72efdd67f2c22627c17d8191850069d4c
gabrielgil245 <gabrielgil245@gmail.com>
2021-03-22 22:24:48 -0500

    Added get and post requests for questions. Requests are displayed to the user.

--- src/App.js
+++ src/App.js
@@ -6,13 +6,51 @@ function App() {
 
   const [categories, setCategories] = useState([]);
   const [selectedCategory, setSelectedCategory] = useState();
+  const [questions, setQuestions] = useState([]);
+  const [selectedQuestion, setSelectedQuestion] = useState();
+  const [inputText, setInputText] = useState('');
 
   const fetchCategories = async () => {
-    console.log("This function will fetch the categories");
-    let res = await fetch('http://localhost:3000/api/v1/categories');
+    let res = await fetch(`http://localhost:3000/api/v1/categories`);
     let data = await res.json();
     console.log(data);
     setCategories(data);
+  }
+
+  const fetchQuestions = async () => {
+    console.log(selectedCategory);
+    if(selectedCategory){
+      let res = await fetch(`http://localhost:3000/api/v1/categories/${selectedCategory}/questions`);
+      let data = await res.json();
+      console.log(data);
+      setQuestions(data);
+    }
+    
+  }
+
+  const createNewQuestion = async () => {
+    console.log(selectedCategory);
+    if(selectedCategory){
+      
+      let questionBody = { questionText: inputText }
+
+      let options = {
+        method: 'POST',
+        body: JSON.stringify(questionBody),
+        headers: {}
+      };
+      options.headers["Accept"] = "application/json, text/plain, */*";
+      options.headers["Content-Type"] = "application/json;charset=utf-8";
+      console.log(options);
+      
+      const res = await fetch(`http://localhost:3000/api/v1/categories/${selectedCategory}/questions`, options);
+      let data = await res.json();
+      console.log(data);
+      fetchQuestions();
+      setInputText('');
+      alert("Question successfully created!");
+    }
+    
   }
 
   useEffect(() => {
@@ -20,7 +58,7 @@ function App() {
   }, [])
 
   useEffect(() => {
-    // fetchQuestions() - // TODO - Fetch and show the questions
+    fetchQuestions();
   }, [selectedCategory])
 
   // Empty square brackets leads to running once on load
@@ -29,6 +67,7 @@ function App() {
   
   return (
     <>
+      {/* Navbar */}
       <div className={"grid grid-cols-12"}>
         <div className={"col-span-full border p-4"}>
           <h1 className={'text-center text-3xl'}>
@@ -36,26 +75,43 @@ function App() {
           </h1>
         </div>
       </div>
-        
+      {/* Sidebar */}
       <div className={"grid grid-cols-12"}>
         <div className={"col-span-full md:col-span-3 lg:col-span-2 border p-4"}>
-            <ul>
-              {categories.map((category, index) => {
-                return <li key={index} className={category.id == selectedCategory ? 
-                  "border p-4 cursor-pointer bg-gray-300" : "border p-4 cursor-pointer"} 
-                  onClick={() => {
-                  setSelectedCategory(category.id);
+          <ul>
+            {categories.map((category, index) => {
+              return <li key={index} className={category.id == selectedCategory ? 
+                "border p-4 cursor-pointer bg-gray-300" : "border p-4 cursor-pointer"} 
+                onClick={() => {
+                setSelectedCategory(category.id);
                 }}>
-                  {category.name}
-                </li>
-              })}
-            </ul>
+                {category.name}
+              </li>
+            })}
+          </ul>
         </div>
-            
+        {/* Forum */}
+
         <div className={"col-span-full md:col-span-9 lg:col-span-10 border p-4"}>
-          <h1>
-            Questions List
-          </h1>
+          <ul>
+            <div className={"grid grid-cols-12 gap-4"}>
+              {selectedCategory && <input value={inputText} onChange={(event) => {
+                setInputText(event.currentTarget.value);
+                }} type="text" className={'col-span-full md:col-span-9 lg:col-span-10 border w-full rounded border-gray-300 p-2'}/>}
+                {selectedCategory && <span className={'col-span-full md:col-span-3 lg:col-span-2 bg-green-600 cursor-pointer rounded text-center text-white text-xl md:text-lg p-2'} onClick={createNewQuestion}>New Question</span>}
+            </div>
+            <br/>
+            {questions.map((question, index) => {
+              return <li key={index} className={question.id == selectedQuestion ? 
+                "border p-4 cursor-pointer bg-blue-300" : "border p-4 cursor-pointer"} 
+                onClick={() => {
+                setSelectedQuestion(question.id);
+                }}>
+                  {question.questionText}
+                  {question.Answers.length > 0 && <span> - Number of Answers: {question.Answers.length}</span>}
+              </li>
+            })}
+          </ul>
         </div>
       </div>
     </>
